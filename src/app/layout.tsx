91522293923339c71433e651721529e25f806619
MASHINKA34 <mashinka34jr@gmail.com>
2025-06-25 11:39:23 +0300

    Sorted the project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "../styles/globals.css";
 import '@mantine/core/styles.css';
 import '@mantine/dates/styles.css';
 import { ColorSchemeScript } from '@mantine/core';
